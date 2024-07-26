82c363f7bf5caf8fd71aa89562c8ff9dfe831b65
ChristianSMorales <77903858+ChristianSMorales@users.noreply.github.com>
2024-07-26 04:16:44 -0500

    Update installer.sh

--- Prerequisitos/installer.sh
+++ Prerequisitos/installer.sh
@@ -10,7 +10,7 @@ sudo apt-get install curl -y
 
 # Docker
 sudo apt-get install ca-certificates -y
-sudo install -m 0755 -d /etc/apt/keyrings
+sudo install -m 0755 -d /etc/apt/keyrings -y
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
@@ -19,7 +19,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 sudo usermod -aG docker $USER
 
