d42b35c64c2ac2d69e6b783a89692f51ed9f2459
ChristianSMorales <77903858+ChristianSMorales@users.noreply.github.com>
2024-07-26 04:14:43 -0500

    Update installer.sh

--- Prerequisitos/installer.sh
+++ Prerequisitos/installer.sh
@@ -9,9 +9,18 @@ sudo apt remove curl -y
 sudo apt-get install curl -y
 
 # Docker
-sudo apt install docker-ce -y
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 sudo usermod -aG docker $USER
 
 # Instalar Go (1.22.3)
